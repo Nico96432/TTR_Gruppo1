b204f61bd41631b34581a4682048dd3d7d3f45ce
Bon Daniel <daniel.bon@isitgo.it>
2022-10-26 08:49:16 +0200

    Creata classe carta

---
+++ TTR_Gruppo1/src/ttr_gruppo1/Carta.java
@@ -0,0 +1,25 @@
+
+package ttr_gruppo1;
+
+public class Carta {
+    
+//CartaTreno
+        private String colore;
+        private boolean jolly;
+//CartaObbiettivo
+        //inizio
+        //fine
+        private int punteggio;
+
+    
+    public Carta(){
+    }
+    
+    public Carta(String colore,int punteggio){
+        this.colore=colore;
+        this.punteggio=punteggio;
+    }
+    
+    
+    
+    }
