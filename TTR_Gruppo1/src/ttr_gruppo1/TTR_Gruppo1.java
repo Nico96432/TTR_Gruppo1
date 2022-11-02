de6174ec2765cc43f67cf5b4d9850b19b06ef80e
Bon Daniel <daniel.bon@isitgo.it>
2022-11-02 08:41:24 +0100

    == invece di = su jolly

--- TTR_Gruppo1/src/ttr_gruppo1/TTR_Gruppo1.java
+++ TTR_Gruppo1/src/ttr_gruppo1/TTR_Gruppo1.java
@@ -7,6 +7,8 @@ public class TTR_Gruppo1 {
     
     public static void main(String[] args) {
       
+        //110 carte Treno: 12 di ogni colore: Viola, Bianco, Blu, Giallo, Arancio, Nero, Rosso e Verde, più 14 Locomotive
+        
     }
     
 }
